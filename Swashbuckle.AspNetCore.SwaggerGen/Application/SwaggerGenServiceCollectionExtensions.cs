37e6ac04a29add4e096d38093e879ccdc23020a1
Dure Sameen <duresameenod@hotmail.com>
2019-11-05 16:30:28 +0500

    updated

---
+++ Swashbuckle.AspNetCore.SwaggerGen/Application/SwaggerGenServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ApiDescriptions;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Newtonsoft;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class SwaggerGenServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSwaggerGen(
+            this IServiceCollection services,
+            Action<SwaggerGenOptions> setupAction = null)
+        {
+            // Add Mvc convention to ensure ApiExplorer is enabled for all actions
+            services.Configure<MvcOptions>(c =>
+                c.Conventions.Add(new SwaggerApplicationConvention()));
+
+            // Register generator and it's dependencies
+            services.AddTransient<ISwaggerProvider, SwaggerGenerator>();
+            services.AddTransient<ISchemaGenerator, SchemaGenerator>();
+            services.AddTransient<IApiModelResolver, NewtonsoftApiModelResolver>();
+
+            // Register custom configurators that assign values from SwaggerGenOptions (i.e. high level config)
+            // to the service-specific options (i.e. lower-level config)
+            services.AddTransient<IConfigureOptions<SwaggerGeneratorOptions>, ConfigureSwaggerGeneratorOptions>();
+            services.AddTransient<IConfigureOptions<SchemaGeneratorOptions>, ConfigureSchemaGeneratorOptions>();
+
+            // Used by the <c>dotnet-getdocument</c> tool from the Microsoft.Extensions.ApiDescription.Server package.
+            services.TryAddSingleton<IDocumentProvider, DocumentProvider>();
+
+            if (setupAction != null) services.ConfigureSwaggerGen(setupAction);
+
+            return services;
+        }
+
+        public static void ConfigureSwaggerGen(
+            this IServiceCollection services,
+            Action<SwaggerGenOptions> setupAction)
+        {
+            services.Configure(setupAction);
+        }
+    }
+}
